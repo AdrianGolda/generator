3827678c40b95fe91946d8689259834d374cadb6
Marcin Hopa <marcinhopa98@wp.pl>
2019-11-01 12:26:46 +0100

    Logs added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 from employee_dto import EmployeeFactory
 from client_dto import ClientFactory
 from survey_dto import ConductedSurveyFactory, SurveyFactory
+import logging
 
 NUMBER_OF_RECORDS: int = 10
 CHANGE_PERCENT = 5
@@ -27,30 +28,42 @@ def change_history_data(str_data_type):
     faker = Faker()
     if str_data_type == 'employee':
         with open('employee_file.csv', newline='', mode='r') as csvfile:
+            logging.basicConfig(filename="changes.log",
+                                level=logging.INFO,
+                                format="Change info: employee's %(message)s")
             possible_changes = ['first_name', 'last_name', 'gender', 'education', 'salary']
             reader = csv.DictReader(csvfile)
             employee_data = []
             for row in reader:
                 employee_data.append(row)
-            changed_employee = random.choice(employee_data)
-            value_to_change = random.choice(list(changed_employee.keys()))
-            while value_to_change not in possible_changes:
+            nr_of_records = len(employee_data)
+            for i in range(CHANGE_PERCENT/100 * nr_of_records):
+                changed_employee = random.choice(employee_data)
                 value_to_change = random.choice(list(changed_employee.keys()))
-            if value_to_change == 'first_name':
-                print("Changed first name in " + changed_employee['last_name'])
-                if changed_employee['gender'] == 1:
-                    changed_employee['first_name'] = faker.first_name_male()
-                else:
-                    changed_employee['first_name'] = faker.first_name_female()
-            elif value_to_change == 'last_name':
-                changed_employee['last_name'] = faker.last_name()
-                print("Changed last name in " + changed_employee['first_name'])
-            elif value_to_change == 'gender':
-                changed_employee['gender'] = int(changed_employee['gender']).__xor__(1)
-            elif value_to_change == 'education':
-                changed_employee['education'] = random.choice(education)
-            elif value_to_change == 'salary':
-                changed_employee['salary'] = random.randrange(2000, 5000, 100)
+                while value_to_change not in possible_changes:
+                    value_to_change = random.choice(list(changed_employee.keys()))
+                if value_to_change == 'first_name':
+                    old_name = changed_employee['first_name']
+                    if changed_employee['gender'] == 1:
+                        changed_employee['first_name'] = faker.first_name_male()
+                    else:
+                        changed_employee['first_name'] = faker.first_name_female()
+                    logging.info(f"(id: {changed_employee['employee_id']}) first name changed: {old_name} => {changed_employee['first_name']}")
+                elif value_to_change == 'last_name':
+                    old_last = changed_employee['last_name']
+                    changed_employee['last_name'] = faker.last_name()
+                    logging.info(f"(id: {changed_employee['employee_id']}) last name changed: {old_last} => {changed_employee['last_name']}")
+                elif value_to_change == 'gender':
+                    changed_employee['gender'] = int(changed_employee['gender']).__xor__(1)
+                    logging.info(f"(id: {changed_employee['employee_id']}) gender changed: {int(changed_employee['gender']).__xor__(1)} => {changed_employee['gender']}")
+                elif value_to_change == 'education':
+                    old_education = changed_employee['education']
+                    changed_employee['education'] = random.choice(education)
+                    logging.info(f"(id: {changed_employee['employee_id']}) education degree changed: {old_education} => {changed_employee['education']}")
+                elif value_to_change == 'salary':
+                    old_salary = changed_employee['salary']
+                    changed_employee['salary'] = random.randrange(2000, 5000, 100)
+                    logging.info(f"(id: {changed_employee['employee_id']}) salary changed: {old_salary} => {changed_employee['salary']}")
         with open('employee_file.csv', newline='', mode='w') as csvfile:
             writer = csv.writer(
                 csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
@@ -71,35 +84,56 @@ def change_history_data(str_data_type):
             for employee in employee_data:
                 writer.writerow(employee.values())
     if str_data_type == 'client':
+        logging.basicConfig(filename="changes.log",
+                            level=logging.INFO,
+                            format="Change info: client's %(message)s")
         with open('clients_file.csv', newline='', mode='r') as csvfile:
             possible_changes = ['first_name', 'last_name', 'gender', 'profession', 'has_kids', 'education', 'is_married']
             reader = csv.DictReader(csvfile)
             client_data = []
             for row in reader:
                 client_data.append(row)
-            changed_client = random.choice(client_data)
-            value_to_change = random.choice(list(changed_client.keys()))
-            while value_to_change not in possible_changes:
+            nr_of_records = len(client_data)
+            for i in range(CHANGE_PERCENT / 100 * nr_of_records):
+                changed_client = random.choice(client_data)
                 value_to_change = random.choice(list(changed_client.keys()))
-            if value_to_change == 'first_name':
-                print("Changed first name in " + changed_client['last_name'])
-                if changed_client['gender'] == 1:
-                    changed_client['first_name'] = faker.first_name_male()
-                else:
-                    changed_client['first_name'] = faker.first_name_female()
-            elif value_to_change == 'last_name':
-                changed_client['last_name'] = faker.last_name()
-                print("Changed last name in " + changed_client['first_name'])
-            elif value_to_change == 'gender':
-                changed_client['gender'] = int(changed_client['gender']).__xor__(1)
-            elif value_to_change == 'education':
-                changed_client['education'] = random.choice(education)
-            elif value_to_change == 'profession':
-                changed_client['profession'] = faker.job()
-            elif value_to_change == 'has_kids':
-                changed_client['has_kids'] = int(changed_client['has_kids']).__xor__(1)
-            elif value_to_change == 'is_married':
-                changed_client['is_married'] = int(changed_client['is_married']).__xor__(1)
+                while value_to_change not in possible_changes:
+                    value_to_change = random.choice(list(changed_client.keys()))
+                if value_to_change == 'first_name':
+                    old_name = changed_client['first_name']
+                    if changed_client['gender'] == 1:
+                        changed_client['first_name'] = faker.first_name_male()
+                    else:
+                        changed_client['first_name'] = faker.first_name_female()
+                    logging.info(
+                        f"(id: {changed_client['client_id']}) first name changed: {old_name} => {changed_client['first_name']}")
+                elif value_to_change == 'last_name':
+                    old_last = changed_client['last_name']
+                    changed_client['last_name'] = faker.last_name()
+                    logging.info(
+                        f"(id: {changed_client['client_id']}) last name changed: {old_last} => {changed_client['last_name']}")
+                elif value_to_change == 'gender':
+                    changed_client['gender'] = int(changed_client['gender']).__xor__(1)
+                    logging.info(
+                        f"(id: {changed_client['client_id']}) gender changed: {int(changed_client['gender']).__xor__(1)} => {changed_client['gender']}")
+                elif value_to_change == 'education':
+                    old_education = changed_client['education']
+                    changed_client['education'] = random.choice(education)
+                    logging.info(
+                        f"(id: {changed_client['client_id']}) education degree changed: {old_education} => {changed_client['education']}")
+                elif value_to_change == 'profession':
+                    old_job = changed_client['profession']
+                    changed_client['profession'] = faker.job()
+                    logging.info(
+                        f"(id: {changed_client['client_id']}) job changed: {old_job} => {changed_client['profession']}")
+                elif value_to_change == 'has_kids':
+                    changed_client['has_kids'] = int(changed_client['has_kids']).__xor__(1)
+                    logging.info(
+                        f"(id: {changed_client['client_id']}) kids status changed: {int(changed_client['has_kids']).__xor__(1)} => {changed_client['has_kids']}")
+                elif value_to_change == 'is_married':
+                    changed_client['is_married'] = int(changed_client['is_married']).__xor__(1)
+                    logging.info(
+                        f"(id: {changed_client['client_id']}) marriage status changed: {int(changed_client['is_married']).__xor__(1)} => {changed_client['is_married']}")
         with open('clients_file.csv', newline='', mode='w') as csvfile:
             writer = csv.writer(
                 csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
